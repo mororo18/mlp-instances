0ab50799de2bbee9ea9e71452fb965d3525b19e4
Victor Mororó <victormororo@gmail.com>
2022-08-10 15:59:35 -0300

    add new .rnd

--- rnd_gen/generate_by_inst_list.py
+++ rnd_gen/generate_by_inst_list.py
@@ -1,9 +1,36 @@
 import os
-f = open("../list-all")
 
+list_path = os.path.abspath("../list-all")
+#f = open(list_path)
+
+from os import listdir
+from os.path import isfile, join
+instdir =os.path.abspath("..") 
+rnddir =os.path.abspath("../rnd") 
+
+inst_files = [f.replace('.tsp', '') for f in listdir(instdir) if isfile(join(instdir, f)) and f.find('.tsp') != -1]
+rnd_files = [f.replace('.rnd', '') for f in listdir(rnddir) if isfile(join(rnddir, f))]
+
+numbrs = [str(n) for n in range(0, 10)]
+
+def inst_size(inst):
+    size = int(''.join([char for char in inst if char in numbrs]))
+    return size
+
+SIZE = 1100
+        
+inst_gen = [inst for inst in inst_files if (inst not in rnd_files and inst_size(inst) < SIZE)]
+
+#inst_list = [inst for
+print(inst_gen)
+#exit(0)
 os.chdir("../")
 
-for inst in f:
+for inst in inst_gen:
+    inst += '.tsp'
+    print(inst)
+    #continue
+    #exit(0)
     os.system("./load " + inst)
     os.chdir("rnd_gen")
     os.system("julia main.jl")
